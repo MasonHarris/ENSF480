79249d21743d0cddb9529f3a84610db4f0cf9554
KHasan8 <kylehasan1@gmail.com>
2021-12-06 09:49:06 -0700

    added register properties

--- src/controllers/LandlordController.java
+++ src/controllers/LandlordController.java
@@ -27,12 +27,38 @@ public class LandlordController extends GUIcontroller {
             login.start();
 
         });
+        view.formListener(e -> registerProperty());
         ArrayList<Property> properties = new ArrayList<>();
         properties.add(new Property("Apartment", true, 5, 5, false, 5, "NW", "Listed", "123 street", 5, 5.6));
         properties.add(new Property("House", true, 55, 59, false, 5, "SW", "Listed", "1234 street", 52, 5.62));
         view.addChangeListingListener(e -> {
             view.displayListingChanges(properties);
         });
+    }
+
+    public void registerProperty() {
+        // gets registration values in this order address number, addresss name,
+        // bathrooms, bedrooms, property type, quadrant, furnished
+        String value[] = view.getFormValues();
+        int addressNumber;
+        int bedrooms;
+        int bathrooms;
+        try {
+            addressNumber = Integer.parseInt(value[0]);
+            bedrooms = Integer.parseInt(value[2]);
+            bathrooms = Integer.parseInt(value[3]);
+        } catch (NumberFormatException e) {
+            view.formError("Non number put in number field");
+
+            return;
+
+        }
+
+        if (!isStringAlpha(value[4]) || !isStringAlpha(value[1])) {
+            view.formError("Non alphabetic character put in invalid field");
+            return;
+        }
+
     }
 
 }
--- src/controllers/RenterController.java
+++ src/controllers/RenterController.java
@@ -71,24 +71,4 @@ public class RenterController extends GUIcontroller {
 
 	}
 
-	// checks if a string only contains alphabetical characters
-	public boolean isStringAlpha(String check) {
-		// deals with empty and null strings
-		if (check == null) {
-			return false;
-		} else if (check.length() == 0) {
-			return false;
-		}
-		// iterates each over each character in string to check it's an alphabetical
-		// letter
-		for (int i = 0; i < check.length(); i++) {
-			if (!Character.isLetter(check.charAt(i))) {
-				System.out.println("This is not a letter " + (int) check.charAt(i));
-				return false;
-			}
-		}
-		return true;
-
-	}
-
 }

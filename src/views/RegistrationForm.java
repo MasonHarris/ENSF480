221cd78f4327de229c165ee7b18e3794d1b9f04f
KHasan8 <kylehasan1@gmail.com>
2021-12-06 09:34:40 -0700

    Create RegistrationForm.java

---
+++ src/views/RegistrationForm.java
@@ -0,0 +1,66 @@
+package views;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
+public class RegistrationForm extends JPanel {
+    JButton register;
+    JTextArea addressNumber;
+    JTextArea addressName;
+    JTextArea bathrooms;
+    JTextArea bedrooms;
+    JComboBox<String> quadrant;
+    JComboBox<String> furnished;
+    JTextArea propertyType;
+    JLabel errorLabel;
+
+    public RegistrationForm() {
+        register = new JButton("Register");
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        addressNumber = new JTextArea();
+        bathrooms = new JTextArea();
+        bedrooms = new JTextArea();
+        addressName = new JTextArea();
+        quadrant = new JComboBox<String>(new String[] { "NW", "NE", "SE", "SW" });
+        propertyType = new JTextArea();
+        furnished = new JComboBox<String>(new String[] { "Furnished", "unfurnished" });
+        errorLabel = new JLabel("");
+    }
+
+    public void displayForm() {
+        JLabel address1Label = new JLabel("Address number");
+        add(address1Label);
+        add(addressNumber);
+        JLabel address2Label = new JLabel("Address name");
+        add(address2Label);
+        add(addressName);
+        JLabel bathRoomsLabel = new JLabel("Number of bathrooms");
+        add(bathRoomsLabel);
+        add(bathrooms);
+        JLabel bedRoomsLabel = new JLabel("Number of bedrooms");
+        add(bedRoomsLabel);
+        add(bedrooms);
+        JLabel propetyJLabel = new JLabel("Property type");
+        add(propetyJLabel);
+        add(propertyType);
+        JLabel quadrantLabel = new JLabel("Quadrant");
+        add(quadrantLabel);
+        add(quadrant);
+        JLabel furnishedLabel = new JLabel("Furnished");
+        add(furnishedLabel);
+        add(furnished);
+        add(register);
+        add(errorLabel);
+
+    }
+
+    public void setText(String error) {
+        errorLabel.setText(error);
+        repaint();
+    }
+
+}

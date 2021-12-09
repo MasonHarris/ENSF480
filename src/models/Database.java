199a4f5738245405fd31385df019f1f06910a560
KHasan8 <kylehasan1@gmail.com>
2021-12-08 23:34:49 -0700

    Update notifications

--- src/models/Database.java
+++ src/models/Database.java
@@ -317,26 +317,42 @@ public class Database {
 			return new ArrayList<Property>();
 		}
 	}
-	// registers new property into the Database, given Property object p
 	public void registerProperty(Property p) {
 		try {
-			String query = "INSERT INTO PROPERTY(propertyType, propertyID, isListed, noOfBedrooms, noOfBathrooms, Furnished, cityQuadrant, listingPeriod, landlordUsername, listingState, amountofFee, address, isPaid)";
-			query = query + "Values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			String query = "INSERT INTO PROPERTY(propertyType, propertyID, isListed, noOfBedrooms, noOfBathrooms, Furnished, cityQuadrant, listingPeriod, landlordUsername, listingState, amountofFee, address)";
+			query = query + "Values(?,?,?,?,?,?,?,?,?,?,?)";
 			PreparedStatement statement = connection.prepareStatement(query);
 			statement.setString(1, p.getPropertyType());
-			statement.setInt(2, p.getPropertyId());
-			statement.setBoolean(3, p.isIsListed());
-			statement.setInt(4, p.getNumOfBed());
-			statement.setInt(5, p.getNumOfBath());
-			statement.setBoolean(6, p.isIsFurnished());
-			statement.setString(7, p.getCityQuadrant());
-			statement.setInt(8, p.getListingPeriod());
-			statement.setString(9, p.getLandlordUsername());
-			statement.setString(10, p.getPropertyStatus());
-			statement.setDouble(11, p.getAmountOfFee());
-			statement.setString(12, p.getAddress());
-			statement.setBoolean(13, p.getisPaid());
+			statement.setBoolean(2, p.isIsListed());
+			statement.setInt(3, p.getNumOfBed());
+			statement.setInt(4, p.getNumOfBath());
+			statement.setBoolean(5, p.isIsFurnished());
+			statement.setString(6, p.getCityQuadrant());
+			statement.setInt(7, p.getListingPeriod());
+			statement.setString(8, p.getLandlordUsername());
+			statement.setString(9, p.getPropertyStatus());
+			statement.setDouble(10, p.getAmountOfFee());
+			statement.setString(11, p.getAddress());
 			statement.executeUpdate();
+			// notify applicable renters
+
+			String query2 = "SELECT * FROM NOTIFICATION WHERE noOfBathrooms = ? AND noOfBedrooms = ? AND FURNISHED = ? AND cityQuadrant = ? AND propertyType = ?";
+			PreparedStatement statement2 = connection.prepareStatement(query2);
+			statement2.setInt(1, p.getNumOfBath());
+			statement2.setInt(2, p.getNumOfBed());
+			statement2.setBoolean(3, p.getIsFurnished());
+			statement2.setString(4, p.getCityQuadrant());
+			statement2.setString(5, p.getPropertyType());
+			ResultSet res = statement2.executeQuery();
+			while (res.next()) {
+				String update = "INSERT INTO NOTIFICATION_RENTER(Username, property_id)Values(?,?)";
+				PreparedStatement statement3 = connection.prepareStatement(update);
+				statement3.setString(1, res.getString("Username"));
+				statement3.setInt(2, p.getPropertyId());
+				statement3.executeUpdate();
+
+			}
+
 		} catch (SQLException e) {
 			System.out.println(e);
 			System.exit(0);
@@ -438,6 +454,8 @@ public class Database {
 	public void subscribeNotification(String user, int noOfBath, int noOfBed, Boolean Furnished, String cityQuadString,
 			String propertyType) {
 		try {
+			//removes previous subscription
+			unsubscribeNotification(user);
 			String query = "INSERT INTO NOTIFICATION(Username, noOfBedrooms, noOfBathrooms, Furnished, cityQuadrant, propertyType)";
 			query = query + "Values(?,?,?,?,?,?)";
 			PreparedStatement statement = connection.prepareStatement(query);
